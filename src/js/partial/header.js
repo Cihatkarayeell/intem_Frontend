cbae01a577f89ff21c9e3a36bd38e22eb3991a02
Cihat <cihatkarayel@icloud.com>
2024-10-17 19:50:14 +0300

    bugs will fix

--- src/js/partial/header.js
+++ src/js/partial/header.js
@@ -1,3 +1,3 @@
 document.querySelector('#searchButton').addEventListener('click', function() {
-    this.querySelector('.search-box').classList.add('active');
+    this.querySelector('.search-box').classList.toggle('active');
 });
